0c6892674458a0ade2c85b155fb617065eae13b4
roney2003 <suryanshronak3@gmail.com>
2024-08-29 21:45:22 +0530

    commit: 02.day2.js

---
+++ 02.day2.js
@@ -0,0 +1,21 @@
+/** aaj main seekhunga ki js main code kaise likhein*/
+
+const studentName = "Rahul";
+let studentEmail = "rahul12@gmail.com";
+var studentRollNo = 34;
+
+
+//studentName = "Rahul Kumar" // const dal diya hai toh change nahi hoga
+
+studentEmail = "rahul23@gmail.com"
+
+
+console.log(studentName);
+
+console.table([studentName, studentEmail, studentRollNo]);
+console.log("Student Name is: " , studentName);
+
+
+
+
+
